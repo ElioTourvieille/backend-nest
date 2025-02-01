2ffb014da4dbcd7aff5f8cf459cd9ba873603650
eliotourvieille <e.tourvieille@gmail.com>
2025-02-01 08:16:54 +0100

    Authorize CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://poker-progrid.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   // Enable validation pipes globally with whitelist and transform options
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
